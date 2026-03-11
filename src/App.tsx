c8bc4e1e4890bf4d79be7278fcbfe95ab4092238
adlee05 <cmalampati@gmail.com>
2026-03-11 10:40:16 +0530

    push webapp to remote

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./index.css";
+
+import logo from "./logo.svg";
+import reactLogo from "./react.svg";
+
+export function App() {
+  return (
+    <div className="app">
+      <h1>CI CD with Jenkins</h1>
+      <h2 className="">Charan Malampati</h2>
+      <h2>2023BCS0217</h2>
+    </div>
+  );
+}
+
+export default App;
